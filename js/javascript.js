1e70c6b977126fae3c743113f61e8b01fb415d64
Mike <mbowser@gmail.com>
2020-04-10 14:24:36 -0400

    Updating week 11

--- js/javascript.js
+++ js/javascript.js
@@ -34,3 +34,24 @@ buttons.forEach(button => {
     console.log(resetSlider.defaultValue);
   };
 });
+
+
+
+    // Function to change background color
+    function changeBodyBg(color){
+        document.body.style.background = color;
+    }
+    
+
+   // Function to change section background color
+    function changeSectionBG(color){
+        document.getElementById("section").style.background = color;
+    }
+
+   // Function to change header text color
+    function changeHeaderColor(color) {
+  var elements = document.getElementsByClassName('header'); // get all elements
+  for(var i = 0; i < elements.length; i++){
+    elements[i].style.color = color;
+  }
+}
